b8951d71a19c7b39c9671de4c1ca53b9ad3f76ce
Adarsh Jain <prasad.tambe@ge.com>
2018-01-03 18:29:34 +0530

    JM latest

--- reddit.js
+++ reddit.js
@@ -1,66 +1,57 @@
 angular.module('Example', []).controller('TestController', [
-    '$scope', '$parse',
-    function ($scope,$parse) {
-        $scope.list = [
-            { name : 'Example 1' , id: "parent1" , comment : false },
-            { name : 'Example 2' , id: "parent2" , comment : false },
-            { name : 'Example 3' , id: "parent3" , comment : false },
-        ];
-      	
-      
-        
-      	$scope.addComments =function(item){
-			
-			$scope.div = document.getElementById(item.id);
-			
-			$scope.input = document.createElement("textarea");
-				
-			$scope.button1 = document.createElement("button");
-				$scope.button1.innerHTML="Add Comments";
-				$scope.button1.onclick = function() { 
-				$scope.add(item);
-				 
-				}
-				$scope.input.id="comment" + item.id;
-				$scope.input.name = "post";
-				$scope.input.maxLength = "50";
-				$scope.input.cols = "10";
-				$scope.input.rows = "10";
-				$scope.div.appendChild($scope.input); //appendChild
-				$scope.div.appendChild($scope.button1);
-      
-	
-        };
-		
-    
-        $scope.add = function (item) {
-			//console.log("df",document.getElementById("comment" + item.id).value); 
-			item.children = (item.children || []);
-            var count = item.children.length + 1;
-            /* item.children.push({
-                name : 'fdsfdsffs',
-                id : item.id + 'child' + count
-            }) */
-			console.log(item.children);
-			setTimeout(function(){
-				 item.children.push({
-                name : document.getElementById("comment" + item.id).value,
-                id : item.id + 'child' + count
-            })
-			$scope.$apply();
-			$scope.div.removeChild($scope.input);
-			$scope.div.removeChild($scope.button1);
-			},100)
-			
-        };
-        $scope.addChildren = function (item) {
-        console.log(item);
-            item.subChildren = (item.subChildren || []);
-            var count = item.subChildren.length + 1;
-            item.subChildren.push({
-                name : 'subChildren ' + count
-            })
-        };
-        
+    '$scope', '$parse','$timeout',
+    function ($scope,$parse,$timeout) {
+    	$scope.list = [
+    	               { name : 'At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.', id: "parent1" , comment : false },
+    	               { name : 'At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.' , id: "parent2" , comment : false },
+    	               { name : 'At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.' , id: "parent3" , comment : false },
+    	               ];
+
+
+
+    	$scope.addComments =function(item){
+
+    		$scope.div = document.getElementById(item.id);
+
+    		$scope.input = document.createElement("textarea");
+
+    		$scope.submitComment = document.createElement("button");
+    		$scope.submitComment.innerHTML="Add Comments";
+    		$scope.submitComment.className="submit-comment";
+    		
+    		$scope.submitComment.onclick = function() { 
+    			$scope.add(item);
+
+    		}
+    		$scope.input.id="comment" + item.id;
+    		$scope.input.className = "text-area";
+    		$scope.input.cols = "70";
+    		$scope.input.rows = "6";
+    		$scope.div.appendChild($scope.input); //appendChild
+    		$scope.div.appendChild($scope.submitComment);
+
+
+    	};
+
+
+    	$scope.add = function (item) {
+    		console.log("df",item); 
+    		item.children = (item.children || []);
+    		var count = item.children.length + 1;
+    		console.log(item.children);
+    		$timeout(function(){
+    			item.children.push({
+    				name : document.getElementById("comment" + item.id).value,
+    				id : item.id + 'child' + count
+    			})
+    			$scope.div.removeChild($scope.input);
+    			$scope.div.removeChild($scope.submitComment);
+    			console.log(item.children);
+    		},100)
+    		
+    		
+
+    	};
+
     }
 ]);
